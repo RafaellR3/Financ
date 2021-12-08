7a6c413cf1d2a9fae73ffe8720eb872867c3009f
RafaellR3 <rafaell_r3@hotmail.com>
2021-12-08 17:54:57 -0300

    cloud

--- frontend/src/components/NovoMes/index.tsx
+++ frontend/src/components/NovoMes/index.tsx
@@ -1,24 +1,25 @@
+import axios from "axios";
 import { useCallback, useState } from "react";
+import { Api } from "utils/requests";
 import novo from "../../assests/img/novo.png"
 
 function AdicionarNovoMes() {
 
-    //const { tasks, postInserirNovoMes, getMesRecuperarTodos } = useToDo();
     const [nome, setMesNome] = useState('');
 
-    // const inserirNovoMes = useCallback(async () => {
-    //     await postInserirNovoMes( nome );
-    //     await getMesRecuperarTodos();
-    //     <ListaMes/>;
-    // }, [postInserirNovoMes, getMesRecuperarTodos, nome])
+    const inserirNovoMes = useCallback(async () => {
+        await axios.post(`${Api}/Mes`, {nome })
+    }, [nome])
 
-    //onClick={inserirNovoMes}
+    const onClick = () => {
+        inserirNovoMes()
+    }
 
     return (
         <div >
             <input className="btn btn-light btn-sm center border" value={nome} type="text" onChange={(e) => setMesNome(e.target.value)} />
             <a role="button" >
-                <img src={novo} alt="novo" width="25" />
+                <img src={novo} alt="novo" width="25" onClick={onClick} />
             </a>
         </div>
     )

dc9fdae187c8af01a02c1d3a2409d898cea1c48e
RafaellR3 <rafaell_r3@hotmail.com>
2021-12-16 10:57:43 -0300

    cloud

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,7 +1,7 @@
 console.log('DATA BASE: ',process.env.DATABASE_URL);
 module.exports = {
     "type": "postgres",
-    "url": "postgres://dsfmzzemijfrtw:d29eb60e0cfa3b157132ccd4c6da6290c704dd879ec76b6404ca676cb44e57b9@ec2-54-225-203-79.compute-1.amazonaws.com:5432/d2lmtcosnvt7lu",
+    "url": "postgres://peezgvkknjfauz:78b5939c0d5e63039f566c224957bc7e632a835b3a6c97ed7a45eb2884999cc0@ec2-52-70-205-234.compute-1.amazonaws.com:5432/d4o6tpj7ab6h6h",
     "migrations": [
         "./dist/database/migrations/*.js"
      ],

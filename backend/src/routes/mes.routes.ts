41e56c7a1a0872426104b45e8d65d45bd1bcf297
RafaellR3 <rafaell_r3@hotmail.com>
2022-01-11 17:06:06 -0300

    cloud

--- backend/src/routes/mes.routes.ts
+++ backend/src/routes/mes.routes.ts
@@ -2,8 +2,8 @@ import { Router } from 'express';
 import { MesController } from '../controller/MesController';
 
 const mesRouter = Router();
-
 const mesController = new MesController();
+
 mesRouter.post("/", mesController.handle);
 mesRouter.get("/recuperartodos", mesController.RecuperarTodos);
 
--- backend/src/routes/usuario.route.ts
+++ backend/src/routes/usuario.route.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { UsuarioController } from '../controller/UsuarioController';
 
-const usuarioRoute = Router();
+const usuarioRouter = Router();
 const usuarioController = new UsuarioController();
 
-usuarioRoute.post("/usuario",  usuarioController.handle);
-usuarioRoute.get("/usuario/recuperarpornome",  usuarioController.RecuperarUsuarioPorNome);
-usuarioRoute.get("/usuario/recuperartodos", usuarioController.RecuperarUsuarioTodos);
+usuarioRouter.post("/usuario",  usuarioController.handle);
+usuarioRouter.get("/usuario/recuperarpornome",  usuarioController.RecuperarUsuarioPorNome);
+usuarioRouter.get("/usuario/recuperartodos", usuarioController.RecuperarUsuarioTodos);
 
-export default usuarioRoute;
+export default usuarioRouter;

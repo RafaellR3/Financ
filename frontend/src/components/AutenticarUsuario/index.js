1f2edf4c429fb55ee8ad7438c87cab905c7a2a7e
RafaellR3 <rafaell_r3@hotmail.com>
2022-01-27 16:05:47 -0300

    autenticacao

--- frontend/src/components/AutenticarUsuario/index.js
+++ frontend/src/components/AutenticarUsuario/index.js
@@ -5,93 +5,94 @@ import { login, logout } from "services/auth";
 import { Api } from "utils/requests";
 
 class Autenticarusuario extends React.PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-          email: '',
-          password: '',
-          errors: {},
-        };
-      }
-
-      getCurrentAccountState = () => ({
-        email: this.state.email,
-        password: this.state.password,
-      });
-    
-    onSubmit  = async (event) => {
-        
-    event.preventDefault();
-    
-    const account = this.getCurrentAccountState();
-    const senha= account.password;
-    const email= account.email;
-        try {
-            await axios.post(`${Api}/Login`, { email, senha})
-            .then((response) => {
-                login (response.data);
-               // localStorage.setItem("token",  response.data);
-                window.location.href = '/Home';
-            }).catch((error) => {
-                logout();
-                //localStorage.setItem("token",  null);
-                window.alert("E-mail ou senha inválido, tente novamente.");
-            })  
-        } catch (error) {
-            this.setState({
-              errors: { form: "E-mail ou senha inválido, tente novamente."},
-            });
-          }
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      errors: {},
     };
+  }
 
-    handleChangeFor = (fieldName) => (event) => {
-        this.setState({ [fieldName]: event.target.value });
-      };
-    
-      handleBlurFor = (fieldName) => (event) => {
-        this.setState({
-          errors: {
-            ...this.state.errors,
-          },
-        });
-      };
+  getCurrentAccountState = () => ({
+    email: this.state.email,
+    password: this.state.password,
+  });
 
-      render() {
-        return ( 
-        <div className="container d-flex justify-content-center">
-            <div className="card mt-5 w-50">
-                <div className="card-body">
-                    <form onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                            <h1>Login</h1>
-                    
-                            <InputField
-                            label="E-mail: "
-                            changeHandler={this.handleChangeFor('email')}
-                            blurHandler={this.handleBlurFor('email')}
-                            error={this.state.errors.email}
-                            />
-                        </div>
-                        <div className="form-group">
-                            <InputField
-                            label="Senha: "
-                            type="password"
-                            changeHandler={this.handleChangeFor('password')}
-                            blurHandler={this.handleBlurFor('password')}
-                            error={this.state.errors.password}
-                            />
-                        </div>
-                        
-                        <br></br>
-                    
-                        <input type="submit"className="btn btn-primary"  value="Login" />
-                        <span className="input-error">{this.state.errors.form}</span>
-                    </form>
-                </div>
-            </div>
+  onSubmit = async (event) => {
+
+    event.preventDefault();
+
+    const account = this.getCurrentAccountState();
+    const senha = account.password;
+    const email = account.email;
+    try {
+      await axios.post(`${Api}/Login`, { email, senha })
+        .then((response) => {
+          login(response.data);
+          // localStorage.setItem("token",  response.data);
+          window.location.href = '/Home';
+        }).catch((error) => {
+          logout();
+          //localStorage.setItem("token",  null);
+          window.alert("E-mail ou senha inválido, tente novamente.");
+        })
+    } catch (error) {
+      this.setState({
+        errors: { form: "E-mail ou senha inválido, tente novamente." },
+      });
+    }
+  };
+
+  handleChangeFor = (fieldName) => (event) => {
+    this.setState({ [fieldName]: event.target.value });
+  };
+
+  handleBlurFor = (fieldName) => (event) => {
+    this.setState({
+      errors: {
+        ...this.state.errors,
+      },
+    });
+  };
+
+  render() {
+    return (
+      <div className="container d-flex justify-content-center">
+        <div className="card mt-5 justify-content-center" style={{ backgroundColor: 'rgba(0,0,255, 0.6)' }}>
+          <div className="card-body d-flex justify-content-center">
+            <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+
+                <h2>Login</h2>
+
+                <InputField
+                  label="E-mail: "
+                  changeHandler={this.handleChangeFor('email')}
+                  blurHandler={this.handleBlurFor('email')}
+                  error={this.state.errors.email}
+                />
+              </div>
+              <div className="form-group">
+                <InputField
+                  label="Senha: "
+                  type="password"
+                  changeHandler={this.handleChangeFor('password')}
+                  blurHandler={this.handleBlurFor('password')}
+                  error={this.state.errors.password}
+                />
+              </div>
+
+              <br></br>
+
+              <input type="submit" className="btn btn-primary" value="Acessar" />
+              <span className="input-error">{this.state.errors.form}</span>
+            </form>
+          </div>
         </div>
-        );
-      }
+      </div>
+    );
+  }
 }
 
 export default Autenticarusuario;
--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,13 +1,28 @@
 import logo from 'assests/img/logo.png';
+import Autenticarusuario from 'components/AutenticarUsuario';
 import { Link } from 'react-router-dom';
+import { logout } from 'services/auth';
+import novo from 'assests/img/novo.png';
+
 function NavBar() {
+
+  const onClick = () => {
+    if (window.confirm(`Você tem certeza que deseja sair?`)) {
+      <Autenticarusuario />
+      logout();
+    }
+
+  }
+
   return (
     <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
       <div className="container">
-        <nav className="my-2 my-md-0 mr-md-3">
-          <Link to="/Home">
+        <nav className="navbar navbar-expand-sm justify-content-end ">
+          <a href="/Home">
             <img src={logo} alt="FinancR3" width="120" />
-          </Link>
+          </a>
+
+          <a className="navbar-brand" onClick={onClick} href='#'> Sair</a>
         </nav>
       </div>
     </div>
